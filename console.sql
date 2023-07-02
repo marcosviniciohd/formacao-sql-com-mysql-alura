92f5a54b7e823767255d28e697fc34a09de6c0ca
Marcos <marcosviniciohd@hotmail.com>
2023-07-02 18:34:46 -0300

    Utilizando o update

--- console.sql
+++ console.sql
@@ -5,3 +5,32 @@ create database sucos;
 use sucos;
 
 
+create table tbCliente(
+    CPF varchar(11),
+    NOME varchar(100),
+    ENDERECO1 varchar(150),
+    ENDERECO2 varchar(150),
+    BAIRRO varchar(100),
+    CIDADE varchar(100),
+    ESTADO varchar(50),
+    CEP varchar(8),
+    IDADE SMALLINT,
+    SEXO varchar(1),
+    LIMITECREDITO FLOAT,
+    VOLUME_COMPRA FLOAT,
+    PRIMEIRA_COMPRA BIT(1));
+
+create table TABELA_DE_VENDEDORES (
+    MATRICULA VARCHAR(5),
+    NOME VARCHAR(100),
+    PERCENTUAL_COMISSAO FLOAT
+);
+
+CREATE TABLE tbProduto (
+    PRODUTO VARCHAR(20),
+    NOME VARCHAR(150),
+    EMBALAGEM VARCHAR(50),
+    TAMANHO VARCHAR(50),
+    SABOR VARCHAR(50),
+    PRECO_LISTA FLOAT
+);
---
+++ insert.sql
@@ -0,0 +1,27 @@
+use sucos;
+
+insert into tbProduto (PRODUTO,
+                       NOME,
+                       EMBALAGEM,
+                       TAMANHO,
+                       SABOR,
+                       PRECO_LISTA)
+values (1, 'Suco de Laranja', 'Caixa', '1L', 'Laranja', 2.50);
+
+insert into tbVendedores (MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES
+    ('00235', 'Máricio Almeira Silva', 0.8),
+    ('00236', 'Cláudia Morais', 0.8);
+
+
+
+select * from tbVendedores;
+delete from tbVendedores where MATRICULA = '00233';
+
+-- Cláudia Morais (00236) recebeu aumento e sua comissão passou a ser de 11%.
+UPDATE tbVendedores SET PERCENTUAL_COMISSAO = 0.11 WHERE MATRICULA = '00236';
+SELECT * FROM tbVendedores;
+
+SELECT NOME FROM tbVendedores;
+
+UPDATE tbVendedores SET NOME = 'José Geraldo da Fonseca Junior' WHERE MATRICULA = '00233';
